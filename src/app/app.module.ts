9f8b6df7be72d96cd2fdbd65fe1cc94762370987
itsshaikhaj <itsshaikhaj@gmail.com>
2021-11-26 17:22:26 +0530

    worked on @Input() & @Output() decoraters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerDetailComponent } from './customer-detail/customer-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerListComponent,
+    CustomerDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/customer.ts
@@ -0,0 +1,9 @@
+export class Customer {
+
+    customerNo: number=0;
+    name: string="";
+    address: string="";
+    city: string="";
+    state: string="";
+    country: string="";
+}
